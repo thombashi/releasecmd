f602496598c88a95f5ca6080180e71368718372d
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-07-22 15:12:01 +0900

    Bump version

--- releasecmd/__version__.py
+++ releasecmd/__version__.py
@@ -1,6 +1,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = f"Copyright 2018, {__author__}"
 __license__ = "MIT License"
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
