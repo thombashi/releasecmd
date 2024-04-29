c1a9ba7efeb687bff130ada40399564e3929ee11
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-04-30 01:08:26 +0900

    Modify to use setuptools_scm for package build
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ with open("README.rst", encoding=ENCODING) as f:
 
 setuptools.setup(
     name=MODULE_NAME,
-    version=pkg_info["__version__"],
     url=REPOSITORY_URL,
     author=pkg_info["__author__"],
     author_email=pkg_info["__email__"],
